0f3bd6f58a9f83f53b9951cbace58c8adb6e01f0
Timur Elsukov <rasputin.t@list.ru>
2024-08-30 16:46:52 +0300

    fixed the swiper problem

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,15 @@
-
-// window.addEventListener("DOMContentLoaded", () =>{
-  const swiper = new Swiper(".swiper", {
-    loop: false,
-    freeMode: true,
-    slidesPerView: "auto",
-    enabled: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
+const swiper = new Swiper(".swiper", {
+  loop: false,
+  freeMode: true,
+  slidesPerView: "auto",
+  enabled: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
   
-    breakpoints: {
-      320: {
-        loop: false,
-        freeMode: true,
-        slidesPerView: "auto",
-        enabled: true,
-        pagination: {
-          el: ".swiper-pagination",
-        },
-
-      },
-      768: {
-        enabled: false,
-      },
+  breakpoints: {
+    768: {
+      enabled: false,
     },
-  });
-  
-// })
-
+  },
+});
